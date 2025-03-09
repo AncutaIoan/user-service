212986ea4cbfdd1923bfd672a538ad05fc3ecca8
eoancuta <eoancuta@gmail.com>
2025-03-09 16:37:02 +0200

    fix: removed extra test files

--- src/test/kotlin/TestUserServiceApplication.kt
+++
@@ -1,9 +0,0 @@
-import adamicus.UserServiceApplication
-import config.TestcontainersConfiguration
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-	fromApplication<UserServiceApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
