b994ed91cc08b93e6e2fd616b3fa808c968e4db8
eoancuta <eoancuta@gmail.com>
2025-03-10 18:56:23 +0200

    added logic for postgres r2dbc runsql method

---
+++ src/test/kotlin/config/RunSqlExtension.kt
@@ -0,0 +1,36 @@
+package config
+
+import io.r2dbc.spi.ConnectionFactory
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.springframework.core.io.ClassPathResource
+import org.springframework.r2dbc.connection.init.ScriptUtils
+import reactor.core.publisher.Mono
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
+
+class RunSqlExtension : BeforeTestExecutionCallback {
+    override fun beforeTestExecution(context: ExtensionContext?) {
+        val testMethod = context?.testMethod?.orElse(null) ?: return
+        val annotation = testMethod.getAnnotation(RunSql::class.java) ?: return
+        val testInstance = context.testInstance.orElseThrow {
+            RuntimeException("Test instance not found. ${javaClass.simpleName} is supposed to be used in JUnit 5 only!")
+        }
+
+        getConnectionFactory(testInstance)?.let { connectionFactory ->
+            annotation.scripts.forEach { script ->
+                Mono.from(connectionFactory.create())
+                    .flatMap { connection -> ScriptUtils.executeSqlScript(connection, ClassPathResource(script)) }
+                    .block()
+            }
+        }
+    }
+
+    private fun getConnectionFactory(testInstance: Any): ConnectionFactory? {
+        return testInstance::class.memberProperties
+            .find { it.name == "connectionFactory" }
+            ?.apply { isAccessible = true }
+            ?.getter
+            ?.call(testInstance) as? ConnectionFactory
+    }
+}
---
+++ src/test/kotlin/config/TestcontainersIntegrationTest.kt
@@ -0,0 +1,16 @@
+package config
+
+import adamicus.config.R2DBCConfiguration
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.context.annotation.Import
+
+@DataR2dbcTest
+@Target(AnnotationTarget.CLASS)
+@ExtendWith(RunSqlExtension::class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(R2DBCConfiguration::class)
+@Tag("integration-test")
+annotation class TestcontainersIntegrationTest()
